8d8df30fec0b2048b54e4a31bed5bec0a73aa14b
Saptaparna <saptaparna@gmail.com>
2020-06-29 14:32:33 -0500

    Minor modifications to script

--- setup_genprod.sh
+++ setup_genprod.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # use as little of genproductions as possible
-git clone --depth 1 --no-checkout https://github.com/AndreasAlbert/genproductions.git -b CMSDAS2018_26x
+git clone --depth 1 --no-checkout https://github.com/cms-sw/genproductions.git 
 # setup sparse checkout
 cd genproductions
 git config core.sparsecheckout true
 {
 	echo '/Utilities'
 	echo '/bin/MadGraph5_aMCatNLO'
-	echo '!/bin/MadGraph5_aMCatNLO/cards'
+	echo '/bin/MadGraph5_aMCatNLO/cards'
+        echo '/bin/MadGraph5_aMCatNLO/cards/examples'
 	echo '/MetaData'
 } > .git/info/sparse-checkout
 git read-tree -mu HEAD
